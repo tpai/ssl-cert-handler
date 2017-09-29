087cfbccee5d668e1820b9a05d6250437704487f
tonypai <tony77794@gmail.com>
2017-09-29 10:21:20 +0800

    Move aws login command to the init script

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 source scripts/run.sh
+source scripts/func.sh
 
 if [ "$1" == "" ]; then
     echo "> You must input source file!"
     exit 1
 fi
+
+configure_aws_cli
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     RESULT="$RESULT,$line"
